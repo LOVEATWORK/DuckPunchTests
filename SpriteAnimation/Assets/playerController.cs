25d1427937adc7b04425b6e84937d3e0736d1984
Lars Huring <lars.huring@loveatwork.se>
2014-01-14 23:04:27 +0100

    Initial commit
    Nothing yet. Might be something soon.

---
+++ SpriteAnimation/Assets/playerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	private Animator animator;
+
+	public Transform playerReachStart, playerReachEnd;
+
+	public KeyCode duck;
+	public KeyCode punch;
+	public AudioClip punchSound;
+	public AudioClip duckSound;
+
+	public bool hit = false;
+	public bool punching = false;
+	public bool ducking = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		PlayerMovement ();
+		Raycasting ();
+	}
+
+	void PlayerMovement() 
+	{
+
+		if (Input.GetKeyDown (duck)) {
+			ducking = true;
+			animator.CrossFade ("Duck", 1f);
+			audio.PlayOneShot(duckSound);
+			// If opponent is ducking, punch shouldn't hit.
+
+		} else if (Input.GetKeyDown (punch)) {
+			punching = true;
+			animator.CrossFade ("Punch", 1f);
+			audio.PlayOneShot(punchSound);
+		} else {
+			punching = false;
+			ducking = false;
+			animator.SetInteger("Action", 0);		
+		}
+	}
+
+	void Raycasting() 
+	{
+		Debug.DrawLine (playerReachStart.position, playerReachEnd.position, Color.white);
+		hit = Physics2D.Linecast (playerReachStart.position, playerReachEnd.position);
+
+		Debug.Log (hit);
+
+		if (punching == true && hit == true && ducking != true) 
+		{
+			Debug.Log("You punched him!!");
+		}
+
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log (col);
+	}
+}
